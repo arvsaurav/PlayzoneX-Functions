868275f2235b1006a801695596de31ae09e54be0
Saurav Kumar <arvsaurav@gmail.com>
2024-06-07 02:41:10 +0530

    updated header

--- functions/CreateTransaction/src/main.js
+++ functions/CreateTransaction/src/main.js
@@ -16,9 +16,9 @@ export default async({req, res}) => {
         });
 
         // Set CORS headers
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'POST');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        //res.setHeader('Access-Control-Allow-Methods', 'POST');
+        //res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
         // Handle successful payment
         res.send({ clientSecret: paymentIntent.client_secret });
