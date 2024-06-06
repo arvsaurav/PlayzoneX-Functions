2cabb357383423be287a8cb97689627bbeb2dd11
Saurav Kumar <arvsaurav@gmail.com>
2024-06-07 02:57:42 +0530

    updated headers

--- functions/CreateTransaction/src/main.js
+++ functions/CreateTransaction/src/main.js
@@ -23,7 +23,9 @@ export default async({req, res}) => {
 
          // Set CORS headers
         const headers = {
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type'
         };
 
         // Handle successful payment
