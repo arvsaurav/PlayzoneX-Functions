b3858d8f0e40c27cda16e16ea89950507b423559
Saurav Kumar <arvsaurav@gmail.com>
2024-06-07 02:37:34 +0530

    updated functions

--- functions/CreateTransaction/src/main.js
+++ functions/CreateTransaction/src/main.js
@@ -1,38 +1,3 @@
-//import { Client } from 'node-appwrite';
-/*
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-    // Why not try the Appwrite SDK?
-    //
-    // const client = new Client()
-    //    .setEndpoint('https://cloud.appwrite.io/v1')
-    //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    //    .setKey(process.env.APPWRITE_API_KEY);
-
-    // You can log messages to the console
-    log('Hello, Logs!');
-
-    // If something goes wrong, log an error
-    error('Hello, Errors!');
-
-    // The `req` object contains the request data
-    if (req.method === 'GET') {
-        // Send a response with the res object helpers
-        // `res.send()` dispatches a string back to the client
-        return res.send('Hello, World!');
-    }
-
-    // `res.json()` is a handy helper for sending JSON
-    return res.json({
-        motto: 'Build like a team of hundreds_',
-        learn: 'https://appwrite.io/docs',
-        connect: 'https://appwrite.io/discord',
-        getInspired: 'https://builtwith.appwrite.io',
-    });
-};
-*/
-
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
@@ -49,6 +14,11 @@ export default async({req, res}) => {
             amount,
             currency
         });
+
+        // Set CORS headers
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'POST');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
         // Handle successful payment
         res.send({ clientSecret: paymentIntent.client_secret });
