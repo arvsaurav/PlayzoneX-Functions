985053b0df7a3a052a0bfc355385047ed764d6aa
Saurav Kumar <arvsaurav@gmail.com>
2024-06-08 22:48:33 +0530

    updated function

--- functions/CreateTransaction/src/main.js
+++ functions/CreateTransaction/src/main.js
@@ -8,24 +8,24 @@ export default async({req, res, log}) => {
     log(req);
     log(res);
 
+    // Set CORS headers for preflight request
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Content-Type'
+    };
+
     if (req.method === 'OPTIONS') {
         // Set CORS headers for preflight request
-        const headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        };
-        
-        // Respond with empty body for preflight request
         res.send({
             //headers
         }, headers);
         return;
     }
 
-    //const { amount, currency } = req.body;
-    const amount = 1000;
-    const currency = "usd";
+    const { amount, currency } = JSON.parse(req.body);
+    // const amount = 1000;
+    // const currency = "usd";
 
     try {
         const paymentIntent = await stripe.paymentIntents.create({
@@ -40,31 +40,31 @@ export default async({req, res, log}) => {
         //res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
          // Set CORS headers
-        const headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        };
+        // const headers = {
+        //     'Access-Control-Allow-Origin': '*',
+        //     'Access-Control-Allow-Methods': 'POST',
+        //     'Access-Control-Allow-Headers': 'Content-Type'
+        // };
 
         // Handle successful payment
         //res.send({ clientSecret: paymentIntent.client_secret });
         res.send({
             statusCode: 200,
-            //headers,
+            headers,
             body: JSON.stringify({ clientSecret: paymentIntent.client_secret })
         }, headers);
 
-        return res.json({
-            paymentIntent,
-        });
+        // return res.json({
+        //     paymentIntent,
+        // });
 
     } catch (error) {
         // Handle payment failure
-        const mssg = error.message;
-        res.send({ status: 'error', message: error.message });
+        //const mssg = error.message;
+        res.send({ status: 'error', message: error.message }, headers);
 
-        return res.json({
-            mssg
-        });
+        // return res.json({
+        //     mssg
+        // });
     }
 }
