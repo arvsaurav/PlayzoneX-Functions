849f2a50dc556f39bcb5e8b6f41772a1cc83262d
Saurav Kumar <arvsaurav@gmail.com>
2024-06-09 01:12:18 +0530

    updated functions

--- functions/CreateTransaction/src/main.js
+++ functions/CreateTransaction/src/main.js
@@ -77,61 +77,78 @@ export default async({req, res, log}) => {
 }
 */
 import { Client } from 'node-appwrite';
+import Stripe from 'stripe';
 
-// This is your Appwrite function
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+    // Why not try the Appwrite SDK?
+    //
+    // const client = new Client()
+    //    .setEndpoint('https://cloud.appwrite.io/v1')
+    //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    //    .setKey(process.env.APPWRITE_API_KEY);
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+    // You can log messages to the console
+    log('Hello, Logs!');
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+    // If something goes wrong, log an error
+    error('Hello, Errors!');
 
-  // Set CORS headers
-  const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-  };
+    // Set CORS headers
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    };
 
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    // Preflight response for CORS
-    res.send('', 204, headers);
-  }
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        // Preflight response for CORS
+        res.send('', 204, headers);
+    }
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!', 200, headers);
-  }
+    // The `req` object contains the request data
+    // if (req.method === 'GET') {
+    //     // Send a response with the res object helpers
+    //     // `res.send()` dispatches a string back to the client
+    //     return res.send('Hello, World!', 200, headers);
+    // }
 
-  if (req.method === 'POST') {
-    const body = req.body;
+    if (req.method === 'POST') {
+        const body = req.body;
 
-    // Process the request body here
-    log(`Received data: ${JSON.stringify(body)}`);
+        // Process the request body here
+        log(`Received data: ${JSON.stringify(body)}`);
 
-    // Send a JSON response
+        const { amount, currency } = JSON.stringify(body);
+
+        
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount,
+                currency
+            });
+            const clientSecret = paymentIntent.client_secret;
+            return res.send(clientSecret, 200, headers);
+        }
+        catch(error) {
+            log(error)
+        }
+
+        // Send a JSON response
+        // return res.json({
+        // message: 'Data received successfully',
+        // receivedData: body,
+        // }, 200, headers);
+    }
+
+    // `res.json()` is a handy helper for sending JSON
     return res.json({
-      message: 'Data received successfully',
-      receivedData: body,
+        motto: 'Build like a team of hundreds_',
+        learn: 'https://appwrite.io/docs',
+        connect: 'https://appwrite.io/discord',
+        getInspired: 'https://builtwith.appwrite.io',
     }, 200, headers);
-  }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  }, 200, headers);
 };
